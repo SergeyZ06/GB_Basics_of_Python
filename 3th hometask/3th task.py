05a8622d6410c348c07ab8b284fe1e00015c281d
SergeyZ06 <SergeyZ06@yandex.ru>
2020-12-27 22:14:01 +0300

    Выполнил задание урока № 3

---
+++ 3th hometask/3th task.py
@@ -0,0 +1,35 @@
+def my_func(arg1=0, arg2=0, arg3=0):
+    """
+    Функция принимает три позиционных аргумента и возвращает значение суммы двух наибольших аргументов
+
+    :param arg1:    первый позиционный аргумент
+    :param arg2:    второй позиционный аргумент
+    :param arg3:    третий позиционный аргумент
+    :return:        сумма двух наибольших аргументов
+    """
+
+    try:
+        arg1 = float(arg1)
+        arg2 = float(arg2)
+        arg3 = float(arg3)
+    except ValueError:
+        return None
+
+    # список аргументов
+    list_args = [arg1, arg2, arg3]
+
+    # поиск первого максимального аргумента
+    first_max = max(list_args)
+
+    # удаление первого максимального элемента из списка
+    list_args.remove(max(list_args))
+
+    # поиск следующего максимального аргумента
+    second_max = max(list_args)
+
+    """ Ещё можно было сделать через list.sort, но не захотелось :D """
+
+    return first_max + second_max
+
+
+print(my_func(1, 3, 3))
