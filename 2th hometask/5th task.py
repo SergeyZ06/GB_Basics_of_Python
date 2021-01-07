7d9dd805bb3b9b262d93190a47d02e290f469d59
SergeyZ06 <SergeyZ06@yandex.ru>
2021-01-07 18:29:36 +0300

    Решение пятого задания оптимизировано: исключено использование метода .sort()

--- 2th hometask/5th task.py
+++ 2th hometask/5th task.py
@@ -1,13 +1,34 @@
-list_a = [2, 5, 6, 8, 3]
+# Список с исходным рейтингом
+list_a = [7, 5, 3, 3, 2]
 
-list_a.sort(reverse=True)
+print(f'We have a rating:\t{list_a}')
 
-print(f'We have a rating: {list_a}')
+# Переменная для хранения нового элемента рейтинга
+var_a = int(input(f'Add some new integer to the rating: '))
 
-a = input(f'Add some new integer to the rating: ')
+# Итератор списка с рейтингом
+iterator = iter(list_a)
+# Переменная для хранения значения итератора
+iterator_value = 0
 
-list_a.append(int(a))
+# Выполнять цикл пока не будет найден элемент списка меньший, чем новый элемент
+# или пока не закончатся элементы списка при переборе списка
+while True:
+    # Пробовать
+    try:
+        # Получить значение очередного элемента
+        iterator_value = int(next(iterator))
+        # Если очередной элемент меньше нового элемента
+        if var_a > iterator_value:
+            # записать новый элемент на позицию текущего
+            list_a.insert(list_a.index(iterator_value), var_a)
+            # прервать цикл
+            break
+    # Если все элементы перебраны
+    except StopIteration:
+        # записать новый элемент в конец списка
+        list_a.append(var_a)
+        # прервать цикл
+        break
 
-list_a.sort(reverse=True)
-
-print(f'New rating is: {list_a}')
+print(f'New rating is:\t\t{list_a}')
