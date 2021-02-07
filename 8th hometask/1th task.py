71f406e6480cac10ef18a77015feb2f6ecc11467
SergeyZ06 <SergeyZ06@yandex.ru>
2021-02-07 21:22:34 +0300

    Решение восьмого практического задания: переделано решение первого пункта задания. Теперь содержимое объекта Дата проверяется в конструкторе. Дополнительный вызов методов проверки не требуется.

--- 8th hometask/1th task.py
+++ 8th hometask/1th task.py
@@ -12,7 +12,20 @@ class Date:
 
     # Конструктор: записывает дату из параметра в атрибут объекта
     def __init__(self, str_data='01-01-1900'):
-        self.str_date = str_data
+        dict_date = Date.date_to_numeric(str_data)
+        dict_date = self.date_validation(dict_date)
+
+        for index in Date.dict_parts_of_date.keys():
+            if dict_date[Date.dict_parts_of_date[index]] is None:
+                print(f'Object "{str_data}" was not created successfully')
+                print(f'Part "{Date.dict_parts_of_date[index]}" of "{str_data}" is not correct')
+                self.dict_date = None
+                break
+            else:
+                self.dict_date = dict_date
+
+        if self.dict_date is not None:
+            print(f'Object "{str_data}" was created successfully')
 
     # Метод Преобразования строчной записи даты в словарь с частями даты
     # Принимает в качестве параметра строковую запись даты
@@ -43,53 +56,45 @@ class Date:
     # Метод Проверки даты
     # Принимает в качестве параметра строковую запись даты
     @staticmethod
-    def date_validation(str_to_validate):
-        # Преобразовать строковую запись даты в словарь с частями даты
-        dict_to_validate = Date.date_to_numeric(str_to_validate)
-
+    def date_validation(dict_to_validate):
         # Пробовать
         try:
             # оценить часть даты "День"
             if dict_to_validate['day'] < 1 or dict_to_validate['day'] > 31:
-                # в случае некорректного значения вывести сообщение
-                print(f'Part "day" of {str_to_validate} is not correct')
+                # в случае некорректного значения убрать значение
+                dict_to_validate['day'] = None
         # в случае невозможности сравнения
         except TypeError:
-            # вывести сообщение
-            print(f'Part "day" of {str_to_validate} is not correct')
+            # убрать значение
+            dict_to_validate['day'] = None
 
         # Аналогичная проверка для части даты "Месяц"
         try:
             if dict_to_validate['month'] < 1 or dict_to_validate['month'] > 12:
-                print(f'Part "month" of {str_to_validate} is not correct')
+                dict_to_validate['month'] = None
         except TypeError:
-            print(f'Part "month" of {str_to_validate} is not correct')
+            dict_to_validate['month'] = None
 
         # Аналогичная проверка для части даты "Год"
         try:
             if dict_to_validate['year'] < 1900 or dict_to_validate['year'] > 2100:
-                print(f'Part "year" of {str_to_validate} is not correct')
+                dict_to_validate['year'] = None
         except TypeError:
-            print(f'Part "year" of "{str_to_validate}" is not correct')
+            dict_to_validate['year'] = None
+
+        return dict_to_validate
 
 
 # Создать объект класса Дата
 new_date = Date(f'{randint(1, 30):02}-{randint(1, 12):02}-{randint(1900, 2100)}')
-# Отобразить содержимое объекта
-print(new_date.str_date)
-# Отобразить результат рабты метода Преобразования
-print(Date.date_to_numeric(new_date.str_date))
-# Вызвать метод Проверки даты
-Date.date_validation(new_date.str_date)
+print(new_date.dict_date)
 
 # Создать объект класса Дата с заведомо некорректным годом
 print()
 new_date_2 = Date(f'01-01-2oo5')
-print(Date.date_to_numeric(new_date_2.str_date))
-Date.date_validation(new_date_2.str_date)
+print(new_date_2.dict_date)
 
 # Создать объект класса Дата с заведомо некорректными днём и годом
 print()
 new_date_3 = Date(f'00-10-2200')
-print(Date.date_to_numeric(new_date_3.str_date))
-Date.date_validation(new_date_3.str_date)
+print(new_date_3.dict_date)
